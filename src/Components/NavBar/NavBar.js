748531c53a3857af7e82845b4d29d5ef01d2a6a6
user <user@users-MacBook-Pro-2.local>
2022-01-27 13:54:31 -0800

    responsive navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import './NavBar.css';
+import React, { useState } from "react";
+import "./NavBar.css";
 const NavBar = () => {
-    return (
-			<nav className="mainNav">
-				<div className="navLogo">
-					<a href="/">
-						<span>P</span>layer
-						<span> D</span>raft
-					</a>
-				</div>
-				<div className="menuLink">
-					<ul>
-						<li>
-							<a href="/">Home</a>
-						</li>
-						<li>
-							<a href="/playerList">Player</a>
-						</li>
-						<li>
-							<a href="/login" className="login">Login</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		);
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	return (
+		<nav className="mainNav">
+			<div className="navLogo">
+				<a href="/">
+					<span>P</span>layer
+					<span>D</span>raft
+				</a>
+			</div>
+			<div className={click ? "menuLink mobileMenuLink" : "menuLink"}>
+				<ul>
+					<li>
+						<a className="nav-links" href="/">
+							Home
+						</a>
+					</li>
+					<li>
+						<a className="nav-links" href="/playerList">
+							Player
+						</a>
+					</li>
+					<li>
+						<a className="nav-links login" href="/login">
+							Login
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="hamburgerMenu" onClick={handleClick}>
+				<i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
